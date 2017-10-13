55294ce0bcf378f87a85c0a2fca01fd4c1a7cd9a
Stefan Reinhold <stefan.reinhold@chello.at>
2017-10-14 01:28:33 +0200

    sooo sleepy, added Reserver, resize, erasing ranges, copy ctor, etc

--- CustomVector/CustomVector.cpp
+++ CustomVector/CustomVector.cpp
@@ -39,44 +39,59 @@ class Vector
 	};
 
 public:
-	// Get some initial memory (maybe ?)
-	Vector();
+	// Initialize Empty Vector (allocates only address space)
+	Vector(void);
+
+	// Make a DEEP Copy of the Vector
+	Vector(const Vector& other);
 
 	// At this point we have to call the dtors of the elements stored in the vector
 	// and release the capacity bytes memory used
-	~Vector();
+	~Vector(void);
 
 	// Return the current size of the vector (element count)
 	size_t size(void) const;
 
-	// Return the allocated size if the vector
+	// Return the capacity of the vector (possible element count)
 	size_t capacity(void) const;
 
 	// Returns wether vector is empty or not
-	bool empty() const;
+	bool empty(void) const;
 
 	// Push back the element into the vector
-	// If capacity is still usable copy the content of object to the next free slot
-	// If capacity is depleted, regrow the internal storage by a efficient pattern (std::vector does * 2)
+	// Uses the objects Copy Constructor, since move semantics are forbidden in this exercise
+	// If capacity is depleted, grow the internal storage by an efficient value (see GetDefaultGrowSize())
 	void push_back(const T& object);
+
 	// Resize the internal storage of the vector 
-	// * if n < el_size:		shrink to fit n (and destruct all elements on the way)
-	// * if n > el_size:		assign values and construct
-	// * if n > allocatiosize:	reallocate more mem till n is reached
-	void resize(size_t n, T object = T());
+	// TODO Talk with team about redundancy of temporary default object by duplicating code, probably useful if T has exspensive ctor
+	// This overload calls resize(size_t newSize, const T& object) with a default temporary object of type T
+	void resize(size_t newSize);
 
-	// Reserve memory in the internal storage to fit at least n elements
-	// If n is lesser than the capacity nothing is done
+	// Resize the internal storage of the vector with default object to use for initialization
+	// * if newSize < size:			destruct all redundant objects, capacity stays the same (vector does not shrink to fit when resizing)
+	// * if newSize > size:			assign values and construct
+	// * if newSize > capacity:		grow to ensure memory for newSize number of elements
+	// new objects of type T are Copy Constructed in place
+	void resize(size_t newSize, const T& object);
+
+	// Reserve memory in the internal storage to fit at least number of newCapacity elements
+	// If newCapacity is lesser than the capacity nothing is done
 	// Reserve shall not alter the containers element
-	void reserve(size_t n);
+	void reserve(size_t newCapacity);
 
-	// 
+	// Erasing one Element
 	void erase(size_t index);
+
+	// Erasing range of elements
+	void erase(size_t startIndex, size_t endIndex);
 
 	// Erase by swap shall work in O(1) by 
 	// * Delete the element at index (dtor)
 	// * Copy the last to its place (placement new + cctor)
 	// * Delete the element at the copied location (dtor)
+	// TODO Currently Done with memmove(), check if we really dont need to reconstruct an intact object.
+	// TODO Object just moves place, pointers inside stay intact.  (shallow vs deep copy)
 	void erase_by_swap(size_t index);
 
 	// Subscript operator to access element at specific location/index
@@ -84,7 +99,14 @@ public:
 	T& operator[] (size_t index);
 
 private:
-	void Grow();
+	//Init funciton initializes virtual address space, does not commit memory
+	void Init();
+
+	//Grows the internal capacity by the grow size rounded up to the next page size
+	void Grow(size_t growSize);
+
+	//Returns the default grow size (Current capacity)
+	size_t GetDefaultGrowSize() const;
 
 	PointerType m_internal_array;
 	size_t m_size;
@@ -97,6 +119,7 @@ private:
 	size_t m_pageSize;
 
 	//Maximum vector capacity as mentioned in lecture - 1GB
+	//TODO Find out if we want to grow adress space too
 	static const size_t MAX_VECTOR_CAPACITY = 1024 * 1024 * 1024;
 };
 
@@ -110,6 +133,12 @@ Vector<T>::Vector()
 
 	m_pageSize = sys_inf.dwPageSize;
 
+	Init();
+}
+
+template<class T>
+void Vector<T>::Init()
+{
 	// Get a block of 1GB MB virtual address space from the OS 
 	m_virtual_memory_begin.as_void = VirtualAlloc(nullptr, MAX_VECTOR_CAPACITY, MEM_RESERVE, PAGE_NOACCESS);
 	m_virtual_memory_end.as_ptr = m_virtual_memory_begin.as_ptr + MAX_VECTOR_CAPACITY;
@@ -121,9 +150,34 @@ Vector<T>::Vector()
 	m_internal_array = m_committed_memory_begin;
 }
 
+template<class T>
+Vector<T>::Vector(const Vector & other)
+	: m_size(0u)
+	, m_capacity(0u)
+	, m_pageSize(other.m_pageSize)
+{
+	//TODO Optimization -> If T trivially contructable use memcpy?
+	Init();
+	reserve(other.capacity);
+
+	for(size_t i = 0; i < other.m_size; ++i)
+	{
+		push_back(other[i]);
+	}
+}
+
+
 template <class T>
 Vector<T>::~Vector()
 {
+	//Desctuction in reverse order
+	for(size_t i = 0u; i < m_size; ++i)
+	{
+		const T* elementToDestruct = &(m_internal_array.as_element[i]);
+		//Destructor called by pointer because of possible virtual destructor (polymorphism)
+		elementToDestruct->~T();
+	}
+
 	const ptrdiff_t memory_size = m_virtual_memory_end.as_ptr - m_virtual_memory_begin.as_ptr;
 	VirtualFree(m_virtual_memory_begin.as_void, memory_size, MEM_FREE);
 }
@@ -146,12 +200,14 @@ bool Vector<T>::empty() const
 	return size() == 0u;
 }
 
+//Using Copy Constructor since move semantics (std::move()) are forbidden for this exercise 
 template <class T>
 void Vector<T>::push_back(const T& object)
 {
 	if(m_capacity == m_size)
 	{
-		Grow();
+		//Grow by Default Size
+		Grow(GetDefaultGrowSize());
 	}
 
 	PointerType targetPtr;
@@ -161,16 +217,64 @@ void Vector<T>::push_back(const T& object)
 	++m_size;
 }
 
+//TODO: Talk about having this overload, and copy code to avoid temporary object
 template <class T>
-void Vector<T>::resize(size_t n, T object = T())
+void Vector<T>::resize(size_t newSize)
 {
+	//Call overload with default constructed object
+	resize(newSize, T());
+}
 
+template<class T>
+void Vector<T>::resize(size_t newSize, const T & object)
+{
+	//If size already equals newSize -> do nothing
+	if (newSize == m_size)
+	{
+		return;
+	}
+
+	//If new size is greater than current size
+	if (newSize > m_size)
+	{
+		if (m_capacity < newSize)
+		{
+			const size_t growSize = newSize - m_capacity;
+	
+			Grow(growSize);
+		}
+
+		PointerType targetPtr;
+		for (size_t i = m_size; i < newSize; ++i)
+		{
+			targetPtr.as_ptr = m_internal_array.as_ptr + i * sizeof(T);
+			new (targetPtr.as_void) T(object);
+		}
+	}
+	else //if new Size is smaller than old size
+	{
+		//Destruct redundant elements
+		for(size_t i = newSize; i < m_size; ++i)
+		{
+			const T* elementToDestruct = &(m_internal_array.as_element[i]);
+			//Destructor called by pointer because of possible virtual destructor (polymorphism)
+			elementToDestruct->~T();
+		}
+	}
+
+	m_size = newSize;
 }
 
 template <class T>
-void Vector<T>::reserve(size_t n)
+void Vector<T>::reserve(size_t newCapacity)
 {
+	//If already big enough, do nothing
+	if(newCapacity < m_capacity)
+	{
+		return;
+	}
 
+	Grow(newCapacity - m_capacity);
 }
 
 template <class T>
@@ -179,6 +283,7 @@ void Vector<T>::erase(size_t index)
 	PointerType destination;
 	destination.as_element = &(m_internal_array.as_element[index]);
 
+	//Destructor called by pointer because of possible virtual destructor (polymorphism)
 	destination.as_element->~T();
 
 	//if not last element, close empty slot
@@ -192,12 +297,50 @@ void Vector<T>::erase(size_t index)
 	--m_size;
 }
 
+//Quote from CPP Reference: The iterator first does not need to be dereferenceable if first==last: erasing an empty range is a no-op.
+//TODO Empty Ranges are a noop? How? Range not knowable at compile time?
+template <class T>
+void Vector<T>::erase(size_t startIndex, size_t endIndex)
+{
+	//Rangechecks are always done in [] operator -> So no extra assert needed
+	if(startIndex == endIndex)
+	{
+		erase(endIndex);
+		return;
+	}
+
+	{
+		const bool isEndBiggerThanStart = endIndex > startIndex;
+		assert("endIndex needs to be larger than startIndex!" && isEndBiggerThanStart);
+	}
+	
+	PointerType destination;
+	destination.as_element = &(m_internal_array.as_element[startIndex]);
+	
+	//Deconstruct including endIndex
+	for(size_t i = startIndex; i <= endIndex; ++i)
+	{
+		const T* elementToDestruct = &(m_internal_array.as_element[i]);
+		//Destructor called by pointer because of possible virtual destructor (polymorphism)
+		elementToDestruct->~T();
+	}
+
+	//if not last element, close empty slots by moving the memory
+	if (endIndex < m_size - 1)
+	{
+		PointerType source;
+		source.as_ptr = m_internal_array.as_ptr + (endIndex + 1) * sizeof(T);
+		memmove(destination.as_void, source.as_void, (m_size - 1 - endIndex) * sizeof(T));
+	}
+}
+
 template <class T>
 void Vector<T>::erase_by_swap(size_t index)
 {
 	PointerType destination;
 	destination.as_element = &(m_internal_array.as_element[index]);
 
+	//Destructor called by pointer because of possible virtual destructor (polymorphism)
 	destination.as_element->~T();
 
 	//if not last element, close empty slot
@@ -220,10 +363,8 @@ T& Vector<T>::operator[](const size_t index)
 }
 
 template<class T>
-void Vector<T>::Grow()
+void Vector<T>::Grow(size_t growSize)
 {
-	size_t growSize = size() * sizeof(T); 
-
 	//Calculate growSize
 	if(growSize != 0u)
 	{
@@ -254,6 +395,12 @@ void Vector<T>::Grow()
 	}
 	
 	m_capacity = (m_committed_memory_end.as_ptr - m_committed_memory_begin.as_ptr) / sizeof(T);
+}
+
+template<class T>
+size_t Vector<T>::GetDefaultGrowSize() const
+{
+	return m_capacity * sizeof(T);
 }
 
 /*
@@ -335,6 +482,45 @@ void TestEraseBySwap()
 	printf("Erase By Swap Test done!\n");
 }
 
+void TestEraseByRange()
+{
+	Vector<size_t> testVector;
+
+	testVector.push_back(123u);
+	testVector.push_back(456u);
+	testVector.push_back(789u);
+	testVector.push_back(123456789u);
+
+	assert(testVector[0] == 123u);
+	assert(testVector[1] == 456u);
+	assert(testVector[2] == 789u);
+	assert(testVector[3] == 123456789u);
+
+	testVector.erase(1, 2);
+
+	assert(testVector[0] == 123u);
+	assert(testVector[1] == 123456789u);
+
+	printf("Erase By Range Test done!\n");
+}
+
+void TestCopyConstructor()
+{
+	Vector<size_t> testVector;
+}
+
+void TestResizing()
+{
+	Vector<size_t> testVector;
+}
+
+void StressTest(size_t count)
+{
+	Vector<size_t> testVector;
+}
+
+//TODO Make better Tests
+//TODO Test with non-default constructors
 int main ()
 {
 	TestPushBack(100000);
@@ -344,6 +530,11 @@ int main ()
 
 	TestErase();
 	TestEraseBySwap();
+	TestEraseByRange();
+
+	//TODO Test Resizing
+	//TODO Test Reserving
+	//TODO Test Copy Constructors
 
 	printf("All Tests done!\n");
 }
